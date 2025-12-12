68407eb7f9280cc979d9bf6fffebf9949063cf90
Romy <romane.pld@gmail.com>
2025-12-12 10:10:26 +0100

    css + responsive

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,20 +1,26 @@
-import Link from 'next/link';
-import Image from 'next/image';
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+  const isHome = pathname === "/";
+
   return (
     <header className="main-header">
       <Link href="/" className="main-header__logo-link">
-
         <Image
-            src="/logo.svg"
-            width={200}
-            height={100}
-            alt="Fisheye Home page"
-            className="main-header__logo"
-          />
+          src="/logo.svg"
+          width={200}
+          height={100}
+          alt="Fisheye Home page"
+          className="main-header__logo"
+        />
       </Link>
-      <h1 className="main-header__title">Nos photographes</h1>
+
+      {isHome && <h1 className="main-header__title">Nos photographes</h1>}
     </header>
   );
 }
